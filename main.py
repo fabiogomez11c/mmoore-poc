f8da07420319b2186f843bb9aca1e9b55e918ccd
Fabio Gomez <fabiogomez11c@gmail.com>
2024-01-15 12:27:49 +0000

    notebook

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from sklearn.metrics import accuracy_score
 import torch
 import pandas as pd
 
+num_labels = 3
+model_ckp = 'distilbert-base-uncased'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
+
+# Load the data
 data = Dataset.from_csv('./synthetic_data.csv')
 cats = {cat: i for i, cat in enumerate(set(data['category']))}
 labels = [cats[i] for i in data['category']]
 data = data.add_column(name='labels', column=labels)
+data = data.shuffle()
+
+# Model and tokenizer
+
 
 print('done')
